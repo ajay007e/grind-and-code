d9c92282e564172d71dfdb59a49c25a4cb5fad60
Ajay Mathew <ajayedathazhe@gmail.com>
2025-09-05 15:19:42 +1000

    Daily Challenge: 4th September 2025

---
+++ leetcode/3516/3516.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        dx = abs(z - x)
+        dy = abs(z - y)
+        if dx == dy:
+            return 0
+        return 1 if dx < dy else 2
