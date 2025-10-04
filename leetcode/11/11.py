5df2ca23aa7d4bd38790da072d35a2c9dc191484
Ajay Mathew <ajayedathazhe@gmail.com>
2025-10-04 14:48:22 +1000

    Daily Challenge: 4th October 2025

---
+++ leetcode/11/11.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i, j, res = 0, len(height) - 1, 0
+        while i < j:
+            res = max(res, (j - i) * min(height[i], height[j]))
+            if height[i] > height[j]:
+                j -= 1
+            else:
+                i += 1
+        return res
