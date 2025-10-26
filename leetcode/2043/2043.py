6acc7f869f52c2ad0bb0e3291dcc97ad045c70d8
Ajay Mathew <ajayedathazhe@gmail.com>
2025-10-26 11:47:18 +1100

    Daily Challenge: 26th October 2025

---
+++ leetcode/2043/2043.py
@@ -0,0 +1,26 @@
+class Bank:
+    def __init__(self, balance: List[int]):
+        self.accounts = balance
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 > len(self.accounts) or account2 > len(self.accounts):
+            return False
+        if self.accounts[account1 - 1] < money:
+            return False
+        self.accounts[account1 - 1] -= money
+        self.accounts[account2 - 1] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account > len(self.accounts):
+            return False
+        self.accounts[account - 1] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account > len(self.accounts):
+            return False
+        if self.accounts[account - 1] < money:
+            return False
+        self.accounts[account - 1] -= money
+        return True
